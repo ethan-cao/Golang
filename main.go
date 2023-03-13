40347a57eb4ca6134e2419786de51c4a55b85f52
ethan-cao <ethancaocgx@gmail.com>
2023-03-13 22:43:39 +0000

    goroutine

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"golang/pkg/basic"
-	"golang/pkg/utils"
 )
 
 // The main() function must appear only once, be in the main() package, and receive and return no arguments.
 func main() {
-	basic.F3()
-	utils.Test1()
-
-	user := basic.User{ID: 1, Name: "ethan", Email: "test"}
-
-	fmt.Println(user.Name)
+	basic.RunLongTask()
 }
---
+++ pkg/basic/goroutine.go
@@ -0,0 +1,42 @@
+package basic
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var waitGroup = sync.WaitGroup{}
+
+func RunLongTask() {
+	// set waitGroup counter to be 2
+	// before spin off threads, add threads to waitGroup,
+	// so the main thread waits for these thread to complete
+	waitGroup.Add(2)
+
+	go runLongTask1()
+	go runLongTask2()
+
+	fmt.Printf("main thread task finshed\n")
+
+	// wait until waitGroup counter to be 0
+	waitGroup.Wait()
+}
+
+func runLongTask1() {
+	time.Sleep(5 * time.Second)
+
+	fmt.Printf("long task1 finshed\n")
+
+	// reduce waitGroup counter by 1
+	waitGroup.Done()
+}
+
+func runLongTask2() {
+	time.Sleep(10 * time.Second)
+
+	fmt.Printf("long task2 finshed\n")
+
+	// reduce waitGroup counter by 1
+	waitGroup.Done()
+}
